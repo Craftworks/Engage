074acc4141ff3884a68043a4017a30bf6fa60444
Craftworks <isam@craft-works.co.jp>
2009-08-02 08:43:33 +0900

    Add Config::Object

---
+++ lib/Engage/Config/Object.pm
@@ -0,0 +1,30 @@
+package Engage::Config::Object;
+
+use Moose;
+with 'Engage::Utils';
+with 'Engage::Config';
+
+__PACKAGE__->meta->make_immutable;
+
+no Moose;
+
+1;
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Craftworks, C<< <craftwork at cpan org> >>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Craftworks, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
